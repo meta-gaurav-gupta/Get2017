cc1b07e18ee7b6b00066cf3c90ed2733a724e218
meta-gaurav-gupta <gaurav.gupta1@metacube.com>
2017-07-17 15:01:27 +0530

    Update PyramidTestSuite.java

--- test/com/metacube/PF/Question1/PyramidTestSuite.java
+++ test/com/metacube/PF/Question1/PyramidTestSuite.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.metacube.PF.Question1;
 
 import org.junit.runner.RunWith;
@@ -10,7 +5,7 @@ import org.junit.runners.Suite;
 
 /**
  *
- * @author User
+ * @author Gaurav
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses(
