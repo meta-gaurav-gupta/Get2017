61ac6626832bc3f4ee846d6ef5fe00243ba402ed
meta-gaurav-gupta <gaurav.gupta1@metacube.com>
2017-07-17 15:01:03 +0530

    Update TestNumbers.java

--- test/com/metacube/PF/Question1/TestNumbers.java
+++ test/com/metacube/PF/Question1/TestNumbers.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.metacube.PF.Question1;
 
 import com.metacube.PF.PyramidMirror;
@@ -12,7 +7,7 @@ import org.junit.Test;
 
 /**
  *
- * @author User
+ * @author Gaurav
  */
 public class TestNumbers {
     /**
@@ -20,14 +15,17 @@ public class TestNumbers {
      */
     @Test
     public void testNumbers() {
+        //Positive Test Case
         int row = 2;
         int n = 5;
         PyramidMirror instance = new PyramidMirror();
         String expResult = "121";
         String result = instance.numbers(row, n);
-        assertEquals(expResult, result);                                        //Positive Test Case
+        assertEquals(expResult, result);        
+        
+        //Negative Test Case
         expResult = "12321";
         result = instance.numbers(row, n);
-        assertNotEquals(expResult, result);                                     //Negative Test Case
+        assertNotEquals(expResult, result);                                     
     }
 }
