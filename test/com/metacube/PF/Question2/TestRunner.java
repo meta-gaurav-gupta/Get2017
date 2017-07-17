a285de7774252f3fdc5f8f22ca9f3bcc2477eef9
meta-gaurav-gupta <gaurav.gupta1@metacube.com>
2017-07-17 15:03:09 +0530

    Update TestRunner.java

--- test/com/metacube/PF/Question2/TestRunner.java
+++ test/com/metacube/PF/Question2/TestRunner.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.metacube.PF.Question2;
 
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 /**
  *
- * @author User
+ * @author Gaurav
  */
 public class TestRunner {
     public static void main(String[] args) {
