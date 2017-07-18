4818bcdf0b9bd08894ee9599bb7ad0308c57e86b
meta-gaurav-gupta <gaurav.gupta1@metacube.com>
2017-07-18 10:26:11 +0530

    Update PyramidMirror.java

--- src/com/metacube/PF/PyramidMirror.java
+++ src/com/metacube/PF/PyramidMirror.java
@@ -46,11 +46,26 @@ public class PyramidMirror {
      */
     public String patternBuilder(int loopCounter, int n){
         StringBuilder result = new StringBuilder();
-        String spaces = spaces(loopCounter, n);
-        result.append(spaces);
-        String numbers = numbers(loopCounter, n);
-        result.append(numbers).append("\n");
-        
+        try {
+            n = Integer.parseInt(n + "");
+            loopCounter = Integer.parseInt(loopCounter + "");
+            if(n < 1) {
+                throw new Exception("Input Must Be Greater Than Zero");
+            }
+            if(loopCounter > n) {
+                throw new Exception("Loop Counter must be smaller than input");
+            }
+            String spaces = spaces(loopCounter, n);
+            result.append(spaces);
+            String numbers = numbers(loopCounter, n);
+            result.append(numbers).append("\n");
+        }
+        catch(NumberFormatException e){
+            System.out.println(e.getMessage());
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
         return result.toString();
     }
     /**
@@ -62,11 +77,28 @@ public class PyramidMirror {
     public String spaces(int row, int n)
     {
         StringBuilder space = new StringBuilder();
-        
-        for (int spaceCounter = 0; spaceCounter < (n - row); spaceCounter++)
-        {
-            space.append(' ');
+        try{
+            n = Integer.parseInt(n + "");
+            row = Integer.parseInt(row + "");
+            if(n < 1) {
+                throw new Exception("Input Must Be Greater Than Zero");
+            }
+            if(row > n) {
+                throw new Exception("Row must be smaller than input");
+            }
+            
+            for (int spaceCounter = 0; spaceCounter < (n - row); spaceCounter++)
+            {
+                space.append(' ');
+            }
         }
+        catch(NumberFormatException e){
+            System.out.println(e.getMessage());
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        
         return space.toString();
     }
 
@@ -78,24 +110,31 @@ public class PyramidMirror {
      */
     public String numbers(int row, int n)
     {
-        if(row<=n) 
-        {
         StringBuilder numbers=new StringBuilder();
-
-        for (int numberCounter = 1; numberCounter <= row; numberCounter++)
-        {
-            numbers.append(numberCounter);
+        try{
+            n = Integer.parseInt(n + "");
+            row = Integer.parseInt(row + "");
+            if(n < 1) {
+                throw new Exception("Input Must Be Greater Than Zero");
+            }
+            if(row > n) {
+                throw new Exception("Row must be smaller than input");
+            }
+            for (int numberCounter = 1; numberCounter <= row; numberCounter++)
+            {
+                numbers.append(numberCounter);
+            }
+            for (int numberCounter = row - 1; numberCounter >= 1; numberCounter--)
+            {
+                numbers.append(numberCounter);
+            }
         }
-        for (int numberCounter = row - 1; numberCounter >= 1; numberCounter--)
-        {
-            numbers.append(numberCounter);
+        catch(NumberFormatException e){
+            System.out.println(e.getMessage());
         }
-        
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
         return numbers.toString();
-        }
-        else 
-        {
-            return "row must be less than pyramid size";
-        }
     }
 }
