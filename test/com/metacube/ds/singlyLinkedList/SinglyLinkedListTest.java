8af241de1531cb563409debc071b50b44562b686
meta-gaurav-gupta <gaurav.gupta1@metacube.com>
2017-09-04 16:35:03 +0530

    Update and rename test/com/metacube/ds/SinglyLinkedList/SinglyLinkedListTest.java to test/com/metacube/ds/singlyLinkedList/SinglyLinkedListTest.java

--- test/com/metacube/ds/singlyLinkedList/SinglyLinkedListTest.java
+++ test/com/metacube/ds/singlyLinkedList/SinglyLinkedListTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.metacube.ds.SinglyLinkedList;
+package com.metacube.ds.singlyLinkedList;
 
 import com.metacube.ds.Utility;
 import java.util.Arrays;
