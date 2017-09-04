dca1ee9810ae649f25bf7edda44e0bddcbfe16a3
meta-gaurav-gupta <gaurav.gupta1@metacube.com>
2017-09-04 16:31:45 +0530

    Update DoublyLinkedList.java

--- src/com/metacube/ds/doublyLinkedList/DoublyLinkedList.java
+++ src/com/metacube/ds/doublyLinkedList/DoublyLinkedList.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.metacube.ds.DoublyLinkedList;
+package com.metacube.ds.doublyLinkedList;
 
 import com.metacube.ds.MyList;
 
